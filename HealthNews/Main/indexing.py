771f7b08d4c9c67eaaaf380837fc108c54a38437
sanjanajayakumar <sanjana2902.sj@gmail.com>
2016-04-06 15:59:42 +0800

    Query formation and execution

--- HealthNews/Main/indexing.py
+++ HealthNews/Main/indexing.py
@@ -2,12 +2,14 @@ from urllib2 import urlopen
 from HealthNews.Main.lemmatization import lemmatization
 import pysolr, os, json
 import simplejson
-
+from nltk.corpus import wordnet as wn
+from itertools import chain
 
 class Indexing(object):
     def __init__(self):
         self.solr = pysolr.Solr('http://localhost:8983/solr/test', timeout=10)
         self.lem = lemmatization()
+        self.resplst=set()
 
     def delete_index(self):
         # Delete existing indexed json files in solr
@@ -58,9 +60,112 @@ class Indexing(object):
             self.solr.add([jsondata])
         self.solr.commit()
 
+    def exec_query(self, url):
+        print ""
+        conn = urlopen(url)
+        rsp = simplejson.load(conn)
+        print rsp
+        for result in rsp["response"]["docs"]:
+             print(result)
+             self.resplst.add(result["docID"][0])
+
     def search(self, query):
-        # fields 'or'
-        # 'http://localhost:8983/solr/test/select?q=keywords:*\:*Obesity*+keywords:*\:*Heart*&wt=json&rows=15'
+
+        #Lemmatized query list
+        queryLst = self.lem.lemmatizeWord(self.lem.removeStopWords(query.lower().split(" ")))
+
+        #keyword AND search#############################
+
+        url="http://localhost:8983/solr/test/select?q="
+        for word in queryLst:
+            url+="%2Bkeywords:*\:*" + word + "*+"
+            #url+="%2Bkeywords:*\:*" + word[0].upper() + word[1:] + "*+"
+        url = url[:-1]
+        url+="&wt=json&rows=15"
+        print "url: " + url
+        self.exec_query(url)
+
+
+        ##headline AND search #############################
+
+        url="http://localhost:8983/solr/test/select?q="
+        for word in queryLst:
+            url+="%2Bheadline:*\:*" + word + "*+"
+            #url+="%2Bheadline:*\:*" + word[0].upper() + word[1:] + "*+"
+
+        url = url[:-1]
+        url+="&wt=json&rows=15"
+        print "url: " + url
+        self.exec_query(url)
+
+        ##keyword + headline OR #############################
+
+        url="http://localhost:8983/solr/test/select?q="
+
+        for word in queryLst:
+            url+="headline:*\:*" + word + "*+" + "keywords:*\:*" + word + "*+"
+            #url+="headline:*\:*" + word[0].upper() + word[1:] + "*+" + "keywords:*\:*" + word[0].upper() + word[1:] + "*+"
+
+        url = url[:-1]
+        url+="&wt=json&rows=15"
+        print "url: " + url
+        self.exec_query(url)
+
+        ##phrase search #############################
+
+        url="http://localhost:8983/solr/test/select?q=headline:*\:\""
+        for word in queryLst:
+            url+= word + "+"
+            #url+=word[0].upper() + word[1:] + "+"
+
+        url = url[:-1]
+        url+="\"&wt=json&rows=15"
+        print "url: " + url
+        self.exec_query(url)
+
+
+        ## SYNONYM headline OR search
+
+
+        print "New list"
+        for item in queryLst:
+            synonyms = wn.synsets(item)
+            lst = set(chain.from_iterable([word.lemma_names() for word in synonyms]))
+
+            url="http://localhost:8983/solr/test/select?q="
+            for word in lst:
+                url+="%2Bheadline:*\:*" + word + "*+"
+                #url+="headline:*\:*" + word[0].upper() + word[1:] + "*+"
+
+            url = url[:-1]
+            url+="&wt=json&rows=15"
+            print "url: " + url
+            self.exec_query(url)
+
+        #Return the list of docIDs of relevant documents
+        return self.resplst
+
+
+
+
+
+
+
+
+
+
+
+"""
+    lem_query = lemmatization()
+        queryLst = lem_query.lemmatizeWord(lem_query.removeStopWords(query.lower().split(" ")))
+        synList = []
+        for obj in queryLst:
+            synList = wn.synsets(obj)
+            for syn in synList:
+                print syn
+
+        url = "'http://localhost:8983/solr/test/select?q=%2Bheadline:*\:*Research*+%2Bkeywords:*\:*Obesity*&wt=json&rows=15'"
+
         conn = urlopen(query)
         rsp = simplejson.load(conn)
         print len(rsp["response"]["docs"])
@@ -71,11 +176,20 @@ class Indexing(object):
         #    print(result)
 
 """
+
+"""
 obj = Indexing()
 print(obj.search("http://localhost:8983/solr/test/select?q=health&wt=json&rows=9999"))
 """
-# conn = urlopen('http://localhost:8983/solr/test/select?q=keywords:*\:*Obesity*&wt=json&rows=15&fl=keywords')
 
+
+
+
+
+
+
+# conn = urlopen('http://localhost:8983/solr/test/select?q=keywords:*\:*Obesity*&wt=json&rows=15&fl=keywords')
+#url:             http://localhost:8983/solr/test/select?q=keywords:*\:*obesity*+keywords:*\:*heart*&wt=json&rows=15
 # # fields 'and'
 # conn = urlopen(
 #     'http://localhost:8983/solr/test/select?q=%2Bheadline:*\:*Research*+%2Bkeywords:*\:*Obesity*&wt=json&rows=15')
