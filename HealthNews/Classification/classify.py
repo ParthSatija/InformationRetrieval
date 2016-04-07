c6d432ed323ddc24383b4be859d953615fd9ae81
Shubham <shubham003@e.ntu.edu.sg>
2016-04-07 17:29:36 +0800

    Removed pkl files

--- HealthNews/Classification/classify.py
+++ HealthNews/Classification/classify.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
@@ -72,21 +74,111 @@ class classify(object):
                         ("RandomForest", model6), ("Bagging", model7), ("GB", model8)], voting='hard',
             weights=[1, 1, 1, 1, 1, 2, 2, 0])
 
-        cv = feature_extraction.text.CountVectorizer(vocabulary=dict)
-        cv = feature_extraction.text.TfidfVectorizer(vocabulary=dict)
-
+        cv1 = feature_extraction.text.CountVectorizer(vocabulary=dict)
+        cv2 = feature_extraction.text.TfidfVectorizer(vocabulary=dict)
+        cv_list = [cv1, cv2]
         model_list = [model1, model2, model3, model4, model5, model6, model7, model8, model9, model10]
         model_used = ["SVM", "LOGISTIC REGRESSION", "GAUSSIAN NB",
                       "MULTINOMIAL NB", "BERNOULLI NB", "RANDOM FOREST", "BAGGING", "GRADIENT",
                       "Voting", "Voting With Weights"]
-
+        cv_used = ["COUNT VECTORIZER", "TFIDF VECTORIZER"]
         joblib.dump(dict, "DICTIONARY")
 
         for counter_model in range(0, len(model_list)):
-            model = model_list[counter_model]
-            X = cv.fit_transform(train_data).toarray()
-            model.fit(X, categ)
-            joblib.dump(model, model_used[counter_model] + ".pkl")  # Save as file
+            for counter_cv in range(0, len(cv_list)):
+                model = model_list[counter_model]
+                cv = cv_list[counter_cv]
+                X = cv.fit_transform(train_data).toarray()
+                model.fit(X, categ)
+                joblib.dump(model, model_used[counter_model] + "_" + cv_used[counter_cv] + ".pkl")  # Save as file
+
+    def classification_results(self):
+        # TEST DATA
+        lem = lemmatization()
+        database_name = "cz4034"
+        table_name = "CZ4034_Original"
+        mysql_object = MySQL()
+        mysql_object.use_database(database_name)
+
+        sql = "(SELECT lead_paragraph, news_desk FROM " + table_name + " WHERE news_desk LIKE \"Travel\" order by DocID desc  LIMIT 25) " \
+                                                                       "UNION " \
+                                                                       "(SELECT lead_paragraph, news_desk FROM " + table_name + " WHERE news_desk LIKE \"dining\" order by DocID desc LIMIT 25) " \
+                                                                                                                                "UNION " \
+                                                                                                                                "(SELECT lead_paragraph, news_desk FROM " + table_name + " WHERE news_desk LIKE \"Politics\" order by DocID desc LIMIT 25);"
+        sql = sql.encode('utf-8')
+        data = mysql_object.execute_query(sql)
+        test_data = []
+        test_categ = []
+        for record in data:
+            test_data.append(record[0])
+            ca = record[1]
+            if (ca.lower() == "travel"):
+                test_categ.append('Travel')
+            elif (ca.lower() == "dining"):
+                test_categ.append('Dining')
+            elif (ca.lower() == "politics"):
+                test_categ.append('Politics')
+        cv_used = ["COUNT VECTORIZER", "TFIDF VECTORIZER"]
+        model_used = ["SVM", "LOGISTIC REGRESSION", "GAUSSIAN NB",
+                      "MULTINOMIAL NB", "BERNOULLI NB", "RANDOM FOREST", "BAGGING", "GRADIENT",
+                      "Voting", "Voting With Weights"]
+        dict = joblib.load("DICTIONARY")
+        cv1 = feature_extraction.text.CountVectorizer(vocabulary=dict)
+        cv2 = feature_extraction.text.TfidfVectorizer(vocabulary=dict)
+        cv_list = [cv1, cv2]
+        for counter_model in range(0, len(model_used)):
+            for counter_cv in range(0, len(cv_used)):
+                model = joblib.load(model_used[counter_model] + "_" + cv_used[counter_cv] + ".pkl")
+                cv = cv_list[counter_cv]
+                Y = cv.fit_transform(test_data).toarray()
+                predicted = model.predict(Y)
+                j = 0
+                travel = 0
+                dining = 0
+                politics = 0
+                y_true = []
+                y_pred = []
+                for i in predicted:
+                    if (test_categ[j] == "Travel"):
+                        if (i == "Travel"):
+                            travel += 1
+                            y_pred.append(0)
+                        elif (i == "Dining"):
+                            y_pred.append(1)
+                        else:
+                            y_pred.append(2)
+                        y_true.append(0)
+                    elif (test_categ[j] == "Dining"):
+                        if (i == "Dining"):
+                            dining += 1
+                            y_pred.append(1)
+                        elif (i == "Travel"):
+                            y_pred.append(0)
+                        else:
+                            y_pred.append(2)
+                        y_true.append(1)
+                    elif (test_categ[j] == "Politics"):
+                        if (i == "Politics"):
+                            politics += 1
+                            y_pred.append(2)
+                        elif (i == "Travel"):
+                            y_pred.append(0)
+                        else:
+                            y_pred.append(1)
+                        y_true.append(2)
+                    j += 1
+                score = precision_recall_fscore_support(y_true, y_pred, average='weighted')
+                print("_______________________")
+                print("MODEL      :  " + model_used[counter_model])
+                print("VECTORIZER :  " + cv_used[counter_cv])
+                print("Travel     :  %d/25" % (travel))
+                print("Dining     :  %d/25" % (dining))
+                print("Politics   :  %d/23" % (politics))
+                print("Precision  :  %.5f" % (score[0]))
+                print("Recall     :  %.5f" % (score[1]))
+                print("F(1) Score :  %.5f" % ((score[1] * score[0] / (score[1] + score[0])) * 2))
+                print("F(W) Score :  %.5f" % (score[2]))
+                print("Accuracy   :  %.5f" % accuracy_score(y_true, y_pred))
 
     def classify_on(self, headline, keyword, content):
         headline = headline.lower()
@@ -106,5 +198,8 @@ class classify(object):
 
 
 c = classify()
-c.classify_on("green potato poisonous", "",
-              "fact sound like joke, perhaps urban legend grew dr. seuss's ''green egg ham.'' food scientist say one myth. reality green potato contain high level toxin, solanine, cause nausea, headache neurological problems")
+c.train_data()
+c.classification_results()
+
+#c.classify_on("green potato poisonous", "",
+#              "fact sound like joke, perhaps urban legend grew dr. seuss's ''green egg ham.'' food scientist say one myth. reality green potato contain high level toxin, solanine, cause nausea, headache neurological problems")
